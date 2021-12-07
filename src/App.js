c4189f31fdc07a9f24332a1a720eec055a1e96b4
ajayvssrinivasa <sriaj711@gmail.com>
2021-12-07 16:15:59 +0530

    pizza front end

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-
 import './App.css';
-import Error from './components/MyErrorBoundary';
-import React,{Suspense} from 'react'
-import Pagenation1 from './components/Pagenation';
-//import SocialLogin from './components/SocialLogin';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
-import Dashboard from './components/Dashboard';
-const  SocialLogin=React.lazy(()=>import('./components/SocialLogin'));
-//import Searching from './components/Search';
-//import { Suspense } from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+import Menu from './components/Menu';
+import ShoppingCart from './components/ShoppingCart';
+import Checkout from './components/Checkout';
+import Order from './components/Order';
+import Allorder from './components/Allorder';
+import Profile from './components/Profile';
+import UpdProfile from './components/UpdProfile';
 function App() {
+   
   return (
-    <div className="App">
-      {/* <Searching/> */}
-      <Error>
-      <Suspense fallback={<div>Loading</div>}>
-        
-        <SocialLogin/>
-      </Suspense>
-      </Error>
-      {/* <Router>
-        <Routes>
-          <Route path="/"  element={<SocialLogin/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/pagination"element={<Pagenation1/>}/>
-        </Routes>
-      
-      </Router> */}
+   
+       <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/dashboard" element={<Menu/>} />
+        <Route path="/cart" element={<ShoppingCart/>} />
+        <Route path="/checkout" element={<Checkout/>} />
+        <Route path="/order" element={<Order/>} />
+        <Route path="/allorders" element={<Allorder/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path="/upd_profile" element={<UpdProfile/>} />
+      </Routes>
+    </Router>
+    </>
     
-    </div>
   );
 }
 
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,55 @@
+import React,{useState, useEffect} from 'react'
+import { Container,Table,Button} from 'react-bootstrap'
+import Nav2 from './Nav2';
+import {getOrder, deleteorder} from './config/Myservice';
+import { useNavigate } from 'react-router';
+export default function ShoppingCart() {
+    let [orders,setOrders] = useState([]);
+   let [total, setTotal] = useState(0);
+    const navigate = useNavigate()
+    useEffect(()=>{
+        getOrder(sessionStorage.getItem('user'))
+        .then(res=>{
+            if(!res.data.msg){
+            setOrders(res.data.orders)
+            const ttl = res.data.orders.reduce((prev, cur)=> prev + (cur.price * cur.quantity),0)
+           setTotal(ttl)
+            }
+        })
+         },[]);
+    const deleteord=(id)=>{
+        deleteorder(id)
+        .then(res=>{
+            alert(res.data.msg)
+            navigate("/cart");
+        })
+    }
+    return (
+        <Container>
+            <Nav2/>
+           <Container className="mt-3">
+           <h3>Shopping Cart</h3>
+           {orders.length!==0 ? 
+           <Table striped bordered hover size="sm">
+                    
+                    <tbody>
+                        {orders.map(item=>
+                        <tr key={item._id}>
+                        <td>{item.pname}</td>
+                        <td>{item.price}</td>
+                        <td>{item.quantity}</td>
+                        <td><Button type="submit" variant="dark" onClick={()=>deleteord(item._id)}>Delete</Button></td>
+                        </tr>
+                        )}
+                        <tr>
+                    <td colSpan="3">${total}</td>
+          
+                    <td><Button href="/checkout" variant="dark" >Check out</Button></td>
+                    </tr>
+                    </tbody>
+            </Table>:<h3 className="mt-5 bg-secondary p-2">Your cart is empty</h3>}
+           </Container>
+            
+        </Container>
+    )
+}
